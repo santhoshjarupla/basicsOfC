5e2a7a6dae1d1e07f5326fd0841e2d00c97eb8fb
santhoshjarupla <santhoshjarupla12@gmail.com>
2024-02-06 21:38:17 +0530

    sumoftwonumberWithoutusingSum.c works for -ve numbers also

--- sumoftwonumberWithoutusingSum.c
+++ sumoftwonumberWithoutusingSum.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 int main()
 {
-    int x = 5,y = 3;
-    
+   
+    //this logic doesn't hold for the -ve numbers
+    #if 0
+     int x = 5,y = 3;
     while(y !=0)
     {
         x++;
         y--;
     }
     printf("sum = %d", x);
+#endif
+    printf("enter the two number to add:\n");
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int sum = 0;
+    if(a>0)
+    {
+        while(a != 0)
+        {
+            b++;
+            a--;
+        }
+    }
+    else if(a<0)
+    {
+        while(a != 0)
+        {
+            a++;
+            b--;
+        }
+    }
+    printf("sum of two numbers  = %d", b);
     return 0;
 }
